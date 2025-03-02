5a6815698c7862e2b1d26d8a65186cac88bbf4f0
Bhumi <bhumishah0604@gmail.com>
2025-03-02 13:25:00 +0530

    java files commited

--- src/Lecture2/Fibonacci.java
+++ src/Lecture2/Fibonacci.java
@@ -1,7 +1,22 @@
 package Lecture2;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = 0; //previous index
+        int i = 1; //current index
+        int count = 2; //since we already have 2 numbers th count starts from 2
 
+        while(count <= n){
+            int temp = i;
+            i = i + p;
+            p = temp;
+            count++;
+        }
+
+        System.out.println(i);
     }
 }
