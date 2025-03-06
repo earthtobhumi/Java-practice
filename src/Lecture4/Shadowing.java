f1f38d08da82ae1f3b7fd59462c2b5226f34cc0c
Bhumi <bhumishah0604@gmail.com>
2025-03-06 06:58:10 +0530

    java files commited

---
+++ src/Lecture4/Shadowing.java
@@ -0,0 +1,15 @@
+package Lecture4;
+
+public class Shadowing {
+    static int x = 90 ; // Instance variable
+    public static void main(String[] args) {
+        System.out.println(x); // 90
+        int x = 40;
+        System.out.println(x); // 40
+        fun();
+    }
+
+    static void fun() {
+        System.out.println(x); // Instance variable
+    }
+}
